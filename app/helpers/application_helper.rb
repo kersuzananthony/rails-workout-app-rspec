0b910c708abb8b9d61868e72fb0129aa7f69331c
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-04-30 16:34:43 +0800

    Improves application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,17 @@ module ApplicationHelper
   def personal_devise_error_messages!
     return '' if resource.errors.empty?
 
+    sentence = "#{resource.errors.count} errors prohibited this #{resource.class.model_name.human.downcase} from being saved:"
     messages = resource.errors.full_messages.map { |msg|
       content_tag :li, msg
     }.join
 
     html = <<-HTML
-      <div class="alert alert-error alert-danger">
-        <a href="#" class="close" data-dismiss="alert">&#215;</a>
-        #{messages}
-      </div>
+    <div class="alert alert-danger alert-block devise-bs">
+      <button type="button" class="close" data-dismiss="alert">&times;</button>
+      <h5>#{sentence}</h5>
+      <ul>#{messages}</ul>
+    </div>
     HTML
 
     html.html_safe
