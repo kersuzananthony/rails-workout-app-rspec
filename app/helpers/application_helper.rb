0c58e78d9481f08b59ae27133a7babedf90ca88c
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-04-30 16:26:27 +0800

    Overrides devise_errors_message! helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def personal_devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg|
+      content_tag :li, msg
+    }.join
+
+    html = <<-HTML
+      <div class="alert alert-error alert-danger">
+        <a href="#" class="close" data-dismiss="alert">&#215;</a>
+        #{messages}
+      </div>
+    HTML
+
+    html.html_safe
+  end
 end
